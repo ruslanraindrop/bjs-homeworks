b42396b4cc28c7b896925d5e3ea1b3b2db1b63ec
ruslanraindrop <inventmeplease@gmail.com>
2020-06-17 13:07:01 +0300

    Complete 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,32 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    
+	'use strict';
+
+    if (isNaN(percent)) {
+    	console.log(`Параметр процентная ставка содержит неправильное значение ${percent}`);
+    	return;
+    } else if (isNaN(contribution)) {
+    	console.log(`Параметр сумма первоначального взноса содержит неправильное значение ${contribution}`);
+    }
+      else if (isNaN(amount)) {
+    	console.log(`Параметр процентная ставка содержит неправильное значение ${amount}`)
+    } else {
+    	let bodyCredit = amount - contribution;
+    	let numberOfMonths = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+    	let P = percent / 100 / 12;
+    	let monthlyCost = bodyCredit * (P + P / (((1 + P) ** numberOfMonths) - 1));
+    	let totalAmount = parseFloat((monthlyCost * numberOfMonths).toFixed(2));
+    	console.log(totalAmount);
+    	return totalAmount;
+    }
+
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    	if ((name === '') || (name === null) || (name === undefined)) {
+    		name = 'Аноним';
+    	}
+		let greeting = `Привет, мир! Меня зовут ${name}`;
+    	return greeting;
 }
+    	
