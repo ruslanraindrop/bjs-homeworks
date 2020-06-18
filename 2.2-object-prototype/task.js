6e53e4e184cd8bc56ef81df0aeb5ef0fb9088f04
ruslanraindrop <inventmeplease@gmail.com>
2020-06-18 19:26:39 +0300

    Complete 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,8 +1,30 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+    let normal = this.toLowerCase().match(/[а-я]/gi).reverse();
+    if (normal.join('') === normal.reverse().join('')) {
+    	return true
+    } else {
+    	return false
+    }
+
+    console.log('А роза упала на лапу Азора'.isPalindrome());
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let roundedAverage;
+    let sum = 0;
+
+    if (marks.length == 0) {
+        return 0   
+    } else {
+        for (i = 0; i < marks.length; i++) {
+            sum += marks[i];
+    }
+        let average = sum / marks.length;
+        roundedAverage = Math.round(average);
+        return roundedAverage
+    }
+
 }
 
 function checkBirthday(birthday) {
